8292a70b8c485bc53a771451b7d4c4cc343d2d55
santiago <saforero88@gmail.com>
2023-02-10 09:24:31 -0500

    :hammer: button simulation

--- src/components/Segments.jsx
+++ src/components/Segments.jsx
@@ -7,8 +7,18 @@ const Segments = () => {
   useEffect(() => {
     if (data) {
       setSegments(JSON.parse(data));
+      converToArray(JSON.parse(data).total_segmentos_por_speaker);
     }
   }, [data]);
+
+  const converToArray = (obj) => {
+    let arr = [];
+    for (let key in obj) {
+      arr.push(`${key}: ${obj[key]}`);
+    }
+    console.log(arr);
+    return arr;
+  };
 
   const Segment = ({ segment }) => (
     <div className="segmentos">
@@ -34,6 +44,19 @@ const Segments = () => {
   return (
     <div className="visualizaction">
       <h1> Visualizacion Segmentos con fragmentos del SRT MUTE FIRE </h1>
+      <h3 className="speaker details">
+        Total de Segementos: {segments?.segmentos?.length}
+      </h3>
+      <h3 className="speaker details">
+        Total de Speakers: {segments?.total_speakers}
+      </h3>
+      <h3 className="speaker details">Total de Segmentos por Speakers:</h3>
+      {converToArray(segments?.total_segmentos_por_speaker).map((item) => (
+        <h3 className="speaker_details" key={item}>
+          {item}
+        </h3>
+      ))}
+
       {segments?.segmentos?.map((segment) => (
         <Segment key={segment.start} segment={segment} />
       ))}
--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,7 +1,8 @@
 import { DataContext } from "@/context/DataContext";
 import axios from "axios";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import rttmJson from "../../public/outputMutefire/mutefire_44000.json";
+import rttmJsonToLive from "../../public/outputToLive/tolive_23000.json";
 
 const Upload = ({ onFileUpload }) => {
   const { setData } = useContext(DataContext);
@@ -10,6 +11,12 @@ const Upload = ({ onFileUpload }) => {
   const [selectedSRT, setSelectedSRT] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
   const [rttmWithSrt, setRttmWithSrt] = useState(null);
+  useEffect(() => {
+    if (rttmWithSrt) {
+      setData(JSON.stringify(rttmWithSrt?.jsonRttm));
+      onFileUpload();
+    }
+  }, [rttmWithSrt]);
 
   const handleUpload = async () => {
     setUploading(true);
@@ -22,43 +29,92 @@ const Upload = ({ onFileUpload }) => {
       console.log(" empieza el post upload");
       const { data } = await axios.post("/api/upload", formData);
       console.log("termina el post upload");
-      console.log(data);
+      console.log(data.filename);
 
       console.log(" empieza el post srtToJson");
       const jsonSrt = await axios.post("/api/srtToJson", {
         filename: data.filename,
       });
+      console.log("termina el post srtToJson");
 
+      console.log(" empieza el post setSpkSrt");
       //leer json de public y mandarlo a setSpkSrt
       const srtWithSpeakers = await axios.post("/api/setSpkSrt", {
         srt: jsonSrt.data.jsonSrt,
         rttm: rttmJson,
       });
+      console.log("termina el post setSpkSrt");
 
+      console.log(" empieza el post setSrtRttm");
       const rrttmWithSrt = await axios.post("/api/setSrtRttm", {
         srt: srtWithSpeakers.data.jsonSrt,
         rttm: rttmJson,
       });
+      console.log("termina el post setSrtRttm");
+      console.log("set rttm with srt");
       setRttmWithSrt(rrttmWithSrt.data);
+      console.log("asignado");
     } catch (error) {
       console.log(error);
     }
     setUploading(false);
   };
-  function handleClick() {
-    handleUpload();
-    if (rttmWithSrt) {
-      setData(JSON.stringify(rttmWithSrt?.jsonRttm));
-      onFileUpload();
+  const handleSetSrt = async (file) => {
+    setUploading(true);
+    try {
+      // console.log(selectedFile);
+      const file_name =
+        file === "mutefire" ? "Mute_Fire_Sub_EN.srt" : "To_Live.srt";
+      const rttmJsonSelected = file === "mutefire" ? rttmJson : rttmJsonToLive;
+
+      console.log(" empieza el post srtToJson mutefire");
+
+      const jsonSrt = await axios.post("/api/srtToJson", {
+        filename: file_name,
+      });
+      console.log("termina el post srtToJson mutefire");
+
+      console.log(" empieza el post setSpkSrt");
+      //leer json de public y mandarlo a setSpkSrt
+      const srtWithSpeakers = await axios.post("/api/setSpkSrt", {
+        srt: jsonSrt.data.jsonSrt,
+        rttm: rttmJsonSelected,
+      });
+      console.log("termina el post setSpkSrt");
+
+      console.log(" empieza el post setSrtRttm");
+      const rrttmWithSrt = await axios.post("/api/setSrtRttm", {
+        srt: srtWithSpeakers.data.jsonSrt,
+        rttm: rttmJsonSelected,
+      });
+      console.log("termina el post setSrtRttm");
+      console.log("set rttm with srt");
+      setRttmWithSrt(rrttmWithSrt.data);
+      console.log("asignado");
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
+
   return (
     <div className="flex items-center justify-center p-12">
-      <div className="mx-auto w-full max-w-[550px] bg-white">
+      <div className="mx-auto w-full max-w-[700px] bg-white">
         <div className="py-6 px-9 bg-slate-900 rounded-lg">
+          <button
+            className="hover:shadow-form w-middle rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
+            onClick={() => handleSetSrt("mutefire")}
+          >
+            Simula la carga del .srt MuteFire
+          </button>
+          <button
+            className="hover:shadow-form w-middle rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none ml-6"
+            onClick={() => handleSetSrt("tolive")}
+          >
+            Simula la carga del .srt To Live
+          </button>
           <div className="mb-6 pt-4">
             <label className="mb-5 block text-xl font-semibold text-[#fff]">
-              Upload .srt Mute Fire
+              Upload .srt
             </label>
 
             <div className="mb-8">
@@ -100,7 +156,7 @@ const Upload = ({ onFileUpload }) => {
           <div>
             <button
               className="hover:shadow-form w-full rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
-              onClick={handleClick}
+              onClick={handleUpload}
             >
               Send File
             </button>
